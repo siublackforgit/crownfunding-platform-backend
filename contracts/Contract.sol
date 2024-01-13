7ea8bc308e8b3a273ee86028e5fcf8e507e2ad23
rickyyeung <rickyyeung@sunnyvision.com>
2024-01-13 21:42:36 +0800

    donate campaign function is created

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -34,4 +34,21 @@ contract MyContract {
         return numberOfCampaigns;
 
     }
+
+    function donateCampaign (uint256 _id) public payable {
+
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value:amount}("");
+
+        if (sent) {
+            campaign.amountCollected += amount;
+        }
+
+    }
 }

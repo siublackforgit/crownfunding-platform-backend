30fa29fa14c31f206b4b601a02b84d0ad81258a9
rickyyeung <rickyyeung@sunnyvision.com>
2024-01-13 21:15:27 +0800

    createCampiagn function created

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,7 +2,36 @@
 pragma solidity ^0.8.9;
 
 contract MyContract {
-    constructor() {}
-}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
 
-// testing
+    mapping (uint256 => Campaign) public campaigns;
+
+    uint256 numberOfCampaigns = 0;
+
+    function createCampaign ( address _owner, string memory _title , string memory  _description, uint256 _target, uint256 _deadline, string memory  _image ) public returns (uint256){
+        
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns;
+
+    }
+}
